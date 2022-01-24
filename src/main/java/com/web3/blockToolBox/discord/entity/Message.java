95f1c554f52ccedac0bcdbf55e2f50d0d1cd7d4f
wangjunsen <wangjunsen@jd.com>
2022-01-24 15:40:26 +0800

    [opt] 消息改动

--- src/main/java/com/web3/blockToolBox/discord/entity/Message.java
+++ src/main/java/com/web3/blockToolBox/discord/entity/Message.java
@@ -11,17 +11,18 @@ import java.util.Random;
 public class Message {
     private static final String [] RANDOM_CONTENT_CN = {
             "冲冲冲", "卷起来", "坚持住", "等白名单", "卷饿了我",
-            "再加把劲冲", "升级真特么慢", "努力肝白名单", "啊这", "你们升得好快啊",
-            "nice", "哈哈哈哈", "优秀", "确实是这样", "666",
+            "再加把劲冲", "活跃起来", "Be active", "努力肝白名单", "啊这",
+            "nice", "哈哈哈哈", "优秀", "确实是这样", "Good Luck",
             "有点累了", "真的么", "想要白名单", "233333", "肝硬化了",
             "慢慢来吧", "加油啊，一起卷", "升得好慢啊", "都是大佬啊",
-            "大佬们别这么肝啊", "肝动天，肝动地，就是肝动不了你"};
+            "大佬们别这么肝啊", "肝动天，肝动地，就是肝动不了你", "I want WL",
+            "666", "Build", "你们升得好快啊", "高质量发言"};
 
     private static final String [] RANDOM_CONTENT_EN = {
             "Chatting all day", "Take your time", "I 'm coming", "Welcome",
             "Let's go!", "Great project", "WAGMI", "Hold on", "Yeah",
             "Nice", "lol", "Good luck", "It's too slow", "Be active",
-            "I 'm tired", "Come on", "Can't stop", "That's right", "This NFT is really nice",
+            "Come on", "Can't stop", "That's right", "This NFT is really nice",
             "To the moon", "I want wl", "GM"};
 
     private String content;
