d23e7a76693654fd70f40f27097d8411c7533d1f
wangjunsen <wangjunsen@jd.com>
2022-04-12 01:22:25 +0800

    [opt] 代码冲突解决

--- src/main/java/com/web3/blockToolBox/discord/service/DiscordService.java
+++ src/main/java/com/web3/blockToolBox/discord/service/DiscordService.java
@@ -3,11 +3,15 @@ package com.web3.blockToolBox.discord.service;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.web3.blockToolBox.common.RestTemplateUtils;
+import com.web3.blockToolBox.discord.entity.BotStartParam;
+import com.web3.blockToolBox.discord.entity.Channel;
 import com.web3.blockToolBox.entity.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
@@ -20,17 +24,30 @@ public class DiscordService {
 
     private static final ScheduledExecutorService timeTaskExecutor = Executors.newScheduledThreadPool(8);
 
-    public void sendCircleMessage(String channelId, String language, long duration, String token) throws InterruptedException {
-        String url = String.format(sendMessageUrlTemplate, channelId);
-        LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>();
+    private static int index = 1;
 
+    public void sendCircleMessage(BotStartParam param) {
+        LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>();
+        Random random = new Random();
         timeTaskExecutor.scheduleAtFixedRate(() -> {
             HttpHeaders headers = new HttpHeaders();
-            headers.add("authorization", token);
+            headers.add("authorization", param.getAuthorization());
             headers.add("content-type", "application/json");
+
+            List<Channel> channels = param.getChannels();
+            String url;
+//            Channel channel;
+//            if (channels.size() == 1) {
+//                channel = channels.get(0);
+//            } else {
+//                channel = channels.get(random.nextInt(channels.size()));
+//            }
+            Channel channel = channels.get(index++ % 2);
+            url = String.format(sendMessageUrlTemplate, channel.getId());
             RestTemplateUtils.postForObject(url,
-                    JSON.toJSONString(Message.produce(queue, language)),
+                    JSON.toJSONString(Message.produce(queue, channel.getLanguage())),
                     headers, JSONObject.class);
-        }, 1000, duration, TimeUnit.MILLISECONDS);
+
+        }, 1000, param.getDuration(), TimeUnit.MILLISECONDS);
     }
 }
